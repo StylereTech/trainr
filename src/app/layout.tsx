b7f45d991b09b71edfd81a2ddad1599c63ab149f
StylereTech <stylere@outlook.com>
2026-04-07 16:32:07 -0500

    fix: remove footer from all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
 import { AuthProvider } from '@/components/providers/AuthProvider'
 import { Navbar } from '@/components/layout/Navbar'
-import { Footer } from '@/components/layout/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -42,7 +41,6 @@ export default function RootLayout({
           <div className="flex min-h-screen flex-col">
             <Navbar />
             <main className="flex-1">{children}</main>
-            <Footer />
           </div>
           <Toaster />
         </AuthProvider>
